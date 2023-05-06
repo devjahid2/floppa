c3c4072123fff8a77324217f97af3b46ba907c81
Jahid <jahidhasan.developer@gmail.com>
2023-05-06 23:02:04 +0700

    update responsive

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -5,7 +5,7 @@ import { images } from '../../assets/images/images';
 
 const Social = () => {
     return (
-        <div className='d-flex flex-column justify-content-md-center flex-md-row align-items-start align-items-md-center justify-content-start gap-3 my-5'>
+        <div className='d-flex flex-column justify-content-md-center flex-md-row align-items-start align-items-md-center justify-content-start gap-3 mt-5'>
             <SocialButton url={'https://domain.com'} img={images.talegram} title={'Telegram'}/>
             <SocialButton url={'https://domain.com'} img={images.twitter} title={'Twitter'}/>
             <SocialButton url={'https://domain.com'} img={images.medium} title={'Medium'}/>
